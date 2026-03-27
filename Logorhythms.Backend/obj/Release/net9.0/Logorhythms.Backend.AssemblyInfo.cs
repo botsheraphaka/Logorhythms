60db0bf108228015526eb7c116f751e1e7dbf466
Goaletsa-Ratsheko <rg22000177@studentmail.biust.ac.bw>
2026-03-27 12:05:15 +0200

    Form Fix

--- Logorhythms.Backend/obj/Release/net9.0/Logorhythms.Backend.AssemblyInfo.cs
+++ Logorhythms.Backend/obj/Release/net9.0/Logorhythms.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logorhythms.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+258e5519e526799b14476a1e33a8cd54baa20aa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7e32fd27ece309490c87d4a7b2e80a43c0ddef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logorhythms.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logorhythms.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
